9b622407d10c8ffd8f7f9b0883a183221eab4f90
fjpaneda <fjpaneda@birt.eus>
2025-07-30 23:08:32 +0200

    cambio estructura

--- script/functions_random_circle.js
+++ script/functions_random_circle.js
@@ -1,20 +1,20 @@
 const circulo = document.getElementById('circulo');
-let mostrar = true;
+var mostrarCirculo = true;
 
 function alternarCirculo() {
-    if (mostrar) {
-    // Mostrar círculo con color aleatorio
-    const color = Math.random() < 0.5 ? 'red' : 'green';
-    circulo.style.backgroundColor = color;
-    circulo.style.display = 'block';
+    if (mostrarCirculo) {
+        // Mostrar círculo con color aleatorio
+        const color = Math.random() < 0.5 ? 'red' : 'green';
+        circulo.style.backgroundColor = color;
+        circulo.style.display = 'block';
     } else {
     // Ocultar el círculo
-    circulo.style.display = 'none';
+        circulo.style.display = 'none';
     }
 
     // Alternar estado para la siguiente vez
-    mostrar = !mostrar;
+    mostrarCirculo = !mostrarCirculo;
 }
 
-// Ejecutar alternarCirculo cada 2 segundos
-setInterval(alternarCirculo, 2000);
+// Ejecutar alternarCirculo cada 1,5 segundos
+setInterval(alternarCirculo, 1500);
